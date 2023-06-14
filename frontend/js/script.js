2a9ccd348a20573d0b90090b06a41203bce355ce
Gustavobvs1 <gustavobvs06@gmail.com>
2023-06-14 16:25:41 -0300

    Adicionando mais coisas no frontend

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,39 @@
 const url = "http://localhost:3000/task";
+const tbody = document.querySelector("tbody");
 
 async function fetchTasks() {
   const res = await fetch(url);
   const tasks = await res.json();
-  console.log(tasks);
+  return tasks;
 }
 
-fetchTasks();
+function createElement(tag, innerText = "", innerHtml = "") {
+  const element = document.createElement(tag);
+  if (innerHtml) {
+    element.innerHTML = innerHtml;
+  }
+  if (innerText) {
+    element.innerText = innerText;
+  }
+
+  return element;
+}
+
+const task = {
+  id: 0,
+  title: "api",
+  status: "pendente",
+  created_at: "19 do 5 de 2921",
+};
+
+function createRow(task) {
+  const { id, title, status, created_at } = task;
+
+  const tr = createElement("tr");
+  const tdTitle = createElement("td", title);
+  const tdStatus = createElement("td");
+  const tdCreatedAt = createElement("td", created_at);
+  const tdActions = createElement("td");
+
+  const editButton = createElement("button");
+}
